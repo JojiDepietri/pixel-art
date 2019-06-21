c294a3831594755f2360bd93c188b32d750989e2
Joji Depietri <jojidepietri@Joji.local>
2019-06-20 22:49:04 -0300

    guia parte 2 sin los últimos 2 puntos

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -24,20 +24,26 @@ var nombreColores = ['White', 'LightYellow',
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
 
-colorPersonalizado.addEventListener('change', 
-  (function() {
+colorPersonalizado.addEventListener('change', (function() {
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
-
+    indicadorDeColor.style.backgroundColor = colorActual;
 
   })
 );
 
+//elementos del DOM
 var paleta = document.getElementById('paleta');
 var grillaPixeles = document.getElementById('grilla-pixeles');
+var cadaColor = document.getElementsByClassName('color-paleta');
+var indicadorDeColor = document.getElementById('indicador-de-color');
+var pixelesGrilla = document.getElementById('grilla-pixeles').children;
+// var botonBorrar = $('#borrar');
 
 
+
+//generar paleta de colores guia 1
 function generarPaletaDeColores() {
   for (var i = 0; i < nombreColores.length; i++) {
     var div = document.createElement('div');
@@ -47,9 +53,47 @@ function generarPaletaDeColores() {
   }
 }
 
+// generar grilla de pixeles guia 1
 function generarGrillaPixeles() {
   for (var i = 0; i < 1750; i++) {
     var div = document.createElement('div');
     grillaPixeles.appendChild(div);
   }
 }
+
+// actualizar indicar de color guia 2
+function eventoIndicadorDeColor() {
+  for (var i = 0; i < cadaColor.length; i++) {
+    cadaColor[i].addEventListener('click', indicarColor);
+  }
+}
+
+function indicarColor(e) {
+  indicadorDeColor.style.backgroundColor = e.target.style.backgroundColor;
+}
+
+// pintar pixeles guia 2
+function eventoPixelesGrilla() {
+  for (var i = 0; i < pixelesGrilla.length; i++) {
+    pixelesGrilla[i].addEventListener('click', pintarPixel);
+  }
+}
+
+function pintarPixel(e) {
+  e.target.style.backgroundColor = indicadorDeColor.style.backgroundColor;
+}
+
+// borrar pixeles guia 3
+// botonBorrar.click(borrarGrilla);
+// function borrarGrilla() {
+//   var $pixelesGrilla = $('#grilla-pixeles div').animate (
+//     {'backgroundColor':'white'}, 500);
+// }
+
+
+// ejecutando funciones
+
+generarGrillaPixeles();
+generarPaletaDeColores();
+eventoIndicadorDeColor();
+eventoPixelesGrilla();
